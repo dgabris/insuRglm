8376eb19e755ad7b624926e6aa6a05744bb58992
realgabon <daniel.gabris@zurich.com>
2019-11-20 10:30:04 +0100

    creating proper package data

--- tests/generate_data.R
+++ tests/generate_data.R
@@ -1,30 +1,89 @@
 library(dplyr)
 
+if(!'insuranceData' %in% installed.packages()) install.packages('insuranceData')
 data(dataCar, package = "insuranceData")
 
+# define functions to generate policy number and split/save into train/test
+generate_pol_nbr <- function(df, num_digits) {
+  pol_nbr <- vector('character', length = nrow(df))
+
+  min_nbr <- as.numeric(paste0('1', paste0(rep('0', num_digits - 1), collapse = '')))
+  max_nbr <- as.numeric(paste0(rep('9', num_digits), collapse = ''))
+  max_range <- max_nbr - min_nbr
+
+  stopifnot(nrow(df) <= max_range)
+
+  max_increment <- floor(max_range / nrow(df))
+
+  current_nbr <- min_nbr
+  for(i in seq_len(nrow(df))) {
+    pol_nbr[[i]] <- as.character(current_nbr)
+
+    current_nbr <- current_nbr + sample(max_increment, 1)
+  }
+
+  orig_cols <- colnames(df)
+
+  df$pol_nbr <- pol_nbr[sample(nrow(df))]
+
+  df[c('pol_nbr', orig_cols)]
+}
+
+split_and_save <- function(df, name, train_frac = 0.8) {
+  train_size <- round(train_frac * nrow(df))
+  train_rows <- sample(1:nrow(df), size = train_size)
+  test_rows <- setdiff(1:nrow(df), train_rows)
+
+  df[train_rows, ] %>% save(file = file.path('data', paste0(name, '_train.rda')))
+  df[test_rows, ] %>% save(file = file.path('data', paste0(name, '_test.rda')))
+}
+
+# create target variables
 my_dummy_data <- dataCar %>%
-  mutate(veh_value = ggplot2::cut_number(veh_value, n = 5)) %>%
-  mutate(
-    frequency = numclaims / exposure,
-    severity = claimcst0 / numclaims,
-    acc_year = sample(2000:2004, size = nrow(.), replace = TRUE)
+  generate_pol_nbr(num_digits = 6) %>%
+  dplyr::mutate(premium = 770 * (veh_value / mean(veh_value)) * (1/ (agecat / median(agecat))) * exposure) %>%
+  dplyr::mutate(
+    pol_yr = sample(2000:2004, size = nrow(.), replace = TRUE),
+    veh_value = ggplot2::cut_number(veh_value, n = 5)
   ) %>%
-  select(-X_OBSTAT_, -clm)
+  dplyr::mutate(
+    freq = numclaims / exposure,
+    sev = if_else(numclaims > 0, claimcst0 / numclaims, 0),
+    bc = claimcst0 / exposure,
+    lr = if_else(premium > 0, claimcst0 / premium, 0),
+    is_clm = clm
+  )
 
-freq_data <- my_dummy_data %>%
-  select(-severity, -numclaims, -claimcst0) %>%
-  mutate(train_test = if_else(runif(n = nrow(.)) <= 0.8, "train", "test")) %>%
-  mutate_at(setdiff(names(.), c("frequency", "exposure")), as.factor) %>%
-  tibble::as.tibble()
+# create multiple datasets
+select_cols <- c(
+  'pol_nbr',
+  'pol_yr',
+  'exposure',
+  'premium',
+  'gender',
+  'agecat',
+  'area',
+  'veh_body',
+  'veh_age',
+  'veh_value'
+)
 
-sev_data <- my_dummy_data %>%
-  select(-frequency, -exposure, -claimcst0) %>%
-  filter(severity > 0) %>%
-  mutate(train_test = if_else(runif(n = nrow(.)) <= 0.8, "train", "test")) %>%
-  mutate_at(setdiff(names(.), c("severity", "numclaims")), as.factor) %>%
-  tibble::as.tibble()
+target_cols <- list(
+  freq = c('numclaims', 'freq'), # TODO - make the poisson GLM work with offset
+  sev = c('sev'),
+  bc = c('bc'),
+  lr = c('lr'),
+  is_clm = c('is_clm')
+)
 
-freq_data %>% filter(train_test == "train") %>% select(-train_test) %>% saveRDS("data/freq_train.rds")
-freq_data %>% filter(train_test == "test") %>% select(-train_test) %>% saveRDS("data/freq_test.rds")
-sev_data %>% filter(train_test == "train") %>% select(-train_test) %>% saveRDS("data/sev_train.rds")
-sev_data %>% filter(train_test == "test") %>% select(-train_test) %>% saveRDS("data/sev_test.rds")
+for(target in c('freq', 'sev', 'bc', 'lr', 'is_clm')) {
+  df_cols <- c(select_cols, target_cols[[target]])
+
+  d <- my_dummy_data
+  if(target == 'sev') {
+    d <- d %>% dplyr::filter(sev > 0)
+  }
+
+  d[df_cols] %>%
+    split_and_save(name = target)
+}
